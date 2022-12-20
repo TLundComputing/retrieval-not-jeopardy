88667b85ffea2506e852e23cabfcf0a4cbf86592
TLundComputing <33316738+TLundComputing@users.noreply.github.com>
2022-12-20 09:20:14 +0000

    Displaying blank buttons in a grid

--- JeopardyButNot/src/main/java/io/github/tlundcomputing/jeopardybutnot/GameViewController.java
+++ JeopardyButNot/src/main/java/io/github/tlundcomputing/jeopardybutnot/GameViewController.java
@@ -1,7 +1,9 @@
 package io.github.tlundcomputing.jeopardybutnot;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -11,12 +13,24 @@ public class GameViewController {
 
     @FXML
     private Label dataToShow;
+    @FXML
+    private GridPane pane;
 
     private ArrayList<String> recievedData;
 
     public void setData(ArrayList<String> theData){
         recievedData = theData;
-        dataToShow.setText(Arrays.toString(recievedData.toArray()));
+    }
+
+    @FXML
+    private void initialize(){
+        Button[][] buttons = new Button[4][3];
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 3; j ++){
+                buttons[i][j] = new Button();
+                pane.add(buttons[i][j], i, j);
+            }
+        }
     }
 
 }
